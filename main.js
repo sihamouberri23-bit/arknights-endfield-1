09d5fa565700d405fef4ef5c71414b0671177dac
sihamouberri23-bit <sihamouberri23@gmail.com>
2026-03-08 23:46:27 +0000

    Update main.js

--- main.js
+++ main.js
@@ -16,3 +16,20 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
+let time = 300;
+
+const timer = setInterval(function(){
+
+let minutes = Math.floor(time / 60);
+let seconds = time % 60;
+
+document.getElementById("timer").innerHTML =
+minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+
+time--;
+
+if(time < 0){
+clearInterval(timer);
+}
+
+},1000);
